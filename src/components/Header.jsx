0be05be0465875e82dbede5959a0ac29e24c48a3
Matheus Doerzbacher <Matheus.doer@gmail.com>
2023-07-14 13:01:43 -0300

    Teste ramificação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="flex px-7 py-1.5 h-12 w-full bg-[#1b1a19] border-b border-[#484644] justify-between">
             <div className="flex flex-col items-center">
-                <h1 className="font-semibold text-lg leading-none ">Minhas Tarefas</h1>
+                <h1 className="font-semibold text-lg leading-none ">Teste</h1>
                 <span className="font-light text-xs ">By: Matheus Doerzbacher</span>
             </div>
             <div className=" text-center w-9 sm:w-64 md:w-96 flex items-center rounded-[4px] bg-[#252423] hover:bg-[#323130]">
