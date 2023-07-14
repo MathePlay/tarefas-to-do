7d37bd398173a08b443dd7b2b3d763c413119dec
MathePLay <Matheus.doer@gmail.com>
2023-07-14 11:15:24 -0300

    Finalizado o Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { Search } from "lucide-react";
 export default function Header() {
     return (
         <header className="flex px-7 py-1.5 h-12 w-full bg-[#1b1a19] border-b border-[#484644] justify-between">
-            <div className="flex flex-col items-center bg-zinc-800 rounded">
+            <div className="flex flex-col items-center">
                 <h1 className="font-semibold text-lg leading-none ">Minhas Tarefas</h1>
-                <span className="font-light text-xs leading-none">By: Matheus Doerzbacher</span>
+                <span className="font-light text-xs ">By: Matheus Doerzbacher</span>
             </div>
             <div className=" text-center w-9 sm:w-64 md:w-96 flex items-center rounded-[4px] bg-[#252423] hover:bg-[#323130]">
                 <form action="" className="flex w-full">
