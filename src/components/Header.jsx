334eb59a35563250b22ad149d728e07cf2006fd1
MathePLay <Matheus.doer@gmail.com>
2023-07-14 11:09:00 -0300

    Ajustes no Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import { Search } from "lucide-react";
 
 export default function Header() {
     return (
-        <header className="flex px-7 py-2 h-12 w-full bg-[#1b1a19] border-b border-[#484644] justify-between">
-            <div className="w-[9rem] flex flex-col items-center">
-                <h1 className="font-semibold text-lg leading-none p-0">Minhas Tarefas</h1>
-                <span className="font-light text-xs">By: Matheus Doerzbacher</span>
+        <header className="flex px-7 py-1.5 h-12 w-full bg-[#1b1a19] border-b border-[#484644] justify-between">
+            <div className="w-[9rem] flex flex-col items-center bg-zinc-800 rounded">
+                <h1 className="font-semibold text-lg leading-none ">Minhas Tarefas</h1>
+                <span className="font-light text-xs leading-none">By: Matheus Doerzbacher</span>
             </div>
-            <div className=" w-9 sm:w-64 md:w-96 flex items-center rounded-[4px] bg-[#252423] hover:bg-[#323130]">
+            <div className=" text-center w-9 sm:w-64 md:w-96 flex items-center rounded-[4px] bg-[#252423] hover:bg-[#323130]">
                 <form action="" className="flex w-full">
                     <button type="submit">
                         <Search size={17} className="mx-2 text-[#55a4f0]" />
