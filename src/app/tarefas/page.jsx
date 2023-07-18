b666a1ba8d4d654d3fbc8ed5c2850c4b0d9f85e1
MathePLay <Matheus.doer@gmail.com>
2023-07-18 00:51:18 -0300

    Retirado os console.log

--- src/app/tarefas/page.jsx
+++ src/app/tarefas/page.jsx
@@ -26,7 +26,6 @@ export default function Tarefas() {
     
     function adicionarTarefa(newTarefa) {
       const idTarefa = Math.floor(Date.now() * Math.random()).toString(36)
-      console.log(newTarefa)
       
       if (newTarefa) {
         const dataAtual = new Date
@@ -37,24 +36,19 @@ export default function Tarefas() {
           concluido: false,
           data: dataAtual.toDateString()
         }
-        console.log(objetoTafefa)
   
         setTarefas([...tarefas, objetoTafefa])
       }
     }
   
     function concluirTarefa(concluirTarefa){
-      console.log("parametro da funcao: ", concluirTarefa)
   
       const indexTarefa = (tarefas.findIndex(item => item.id === concluirTarefa))
-      console.log("index: ", indexTarefa)
       
       const tarefaSelecionada = tarefas[indexTarefa]
       tarefaSelecionada.concluido = true
-      console.log("Tarefa selecionada apos a alteração: ", tarefaSelecionada)
       
       setTarefas(tarefas.filter(item => item.id !== concluirTarefa))
-      console.log("apos o delete: ",tarefas)
       
       setTarefas([...tarefas])
   
